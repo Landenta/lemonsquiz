eb4894910ccf024e74ad55e295c9084c4cb6b641
Ben <lindenfeldben@gmail.com>
2024-12-28 19:11:55 +0200

    more data

---
+++ src/quizData.js
@@ -0,0 +1,478 @@
+export const quizData = {
+    capitals: {
+      1: [
+        {
+          question: "מה היא בירת צרפת?",
+          options: ["לונדון", "מדריד", "פריז", "רומא"],
+          correctAnswer: "פריז"
+        },
+        {
+          question: "מה היא בירת יפן?",
+          options: ["סיאול", "בייג'ינג", "בנגקוק", "טוקיו"],
+          correctAnswer: "טוקיו"
+        },
+        {
+          question: "מה היא בירת ברזיל?",
+          options: ["ברזיליה", "ריו דה ז'נרו", "סאו פאולו", "בואנוס איירס"],
+          correctAnswer: "ברזיליה"
+        },
+        {
+          question: "מה היא בירת אוסטרליה?",
+          options: ["סידני", "מלבורן", "קנברה", "בריסביין"],
+          correctAnswer: "קנברה"
+        },
+        {
+          question: "מה היא בירת מצרים?",
+          options: ["קהיר", "אלכסנדריה", "דובאי", "אמן"],
+          correctAnswer: "קהיר"
+        },
+        {
+          question: "מה היא בירת קנדה?",
+          options: ["טורונטו", "מונטריאול", "ונקובר", "אוטווה"],
+          correctAnswer: "אוטווה"
+        },
+        {
+          question: "מה היא בירת ספרד?",
+          options: ["ברצלונה", "מדריד", "ולנסיה", "סביליה"],
+          correctAnswer: "מדריד"
+        },
+        {
+          question: "מה היא בירת הודו?",
+          options: ["מומבאי", "ניו דלהי", "בנגלור", "כלכותה"],
+          correctAnswer: "ניו דלהי"
+        },
+        {
+          question: "מה היא בירת ארגנטינה?",
+          options: ["בואנוס איירס", "סנטיאגו", "לימה", "מונטווידאו"],
+          correctAnswer: "בואנוס איירס"
+        },
+        {
+          question: "מה היא בירת דרום אפריקה?",
+          options: ["קייפטאון", "יוהנסבורג", "פרטוריה", "דרבן"],
+          correctAnswer: "פרטוריה"
+        }
+      ],
+      2: [
+        {
+          question: "מהי עיר הבירה של פינלנד?",
+          options: ["אוסלו", "הלסינקי", "סטוקהולם", "קופנהגן"],
+          correctAnswer: "הלסינקי"
+        },
+        {
+          question: "מהי עיר הבירה של אירלנד?",
+          options: ["אדינבורו", "קרדיף", "דבלין", "בלפסט"],
+          correctAnswer: "דבלין"
+        },
+        {
+          question: "מהי עיר הבירה של הונגריה?",
+          options: ["זאגרב", "פראג", "וינה", "בודפשט"],
+          correctAnswer: "בודפשט"
+        },
+        {
+          question: "מהי עיר הבירה של דנמרק?",
+          options: ["סטוקהולם", "קופנהגן", "אוסלו", "הלסינקי"],
+          correctAnswer: "קופנהגן"
+        },
+        {
+          question: "מהי עיר הבירה של פורטוגל?",
+          options: ["מדריד", "ברצלונה", "ליסבון", "פורטו"],
+          correctAnswer: "ליסבון"
+        },
+        {
+          question: "מהי עיר הבירה של פולין?",
+          options: ["בוקרשט", "ורשה", "בודפשט", "פראג"],
+          correctAnswer: "ורשה"
+        },
+        {
+          question: "מהי עיר הבירה של יוון?",
+          options: ["סלוניקי", "איסטנבול", "אתונה", "רומא"],
+          correctAnswer: "אתונה"
+        },
+        {
+          question: "מהי עיר הבירה של אוסטריה?",
+          options: ["ברן", "וינה", "ציריך", "מינכן"],
+          correctAnswer: "וינה"
+        },
+        {
+          question: "מהי עיר הבירה של צ'כיה?",
+          options: ["ברטיסלבה", "ורשה", "בודפשט", "פראג"],
+          correctAnswer: "פראג"
+        },
+        {
+          question: "מהי עיר הבירה של נורבגיה?",
+          options: ["קופנהגן", "סטוקהולם", "אוסלו", "הלסינקי"],
+          correctAnswer: "אוסלו"
+        }
+      ]
+    },
+    flags: {
+      1: [
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇯🇵",
+          options: ["סין", "יפן", "וייטנאם", "קוריאה"],
+          correctAnswer: "יפן"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇮🇹",
+          options: ["ספרד", "צרפת", "איטליה", "יוון"],
+          correctAnswer: "איטליה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇧🇷",
+          options: ["ארצנטינה", "ברזיל", "קולומביה", "צ'ילה"],
+          correctAnswer: "ברזיל"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇬🇧",
+          options: ["בריטניה", "אוסטרליה", "ניו זילנד", "קנדה"],
+          correctAnswer: "בריטניה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇨🇦",
+          options: ["קנדה", "ארה״ב", "מקסיקו", "אלסקה"],
+          correctAnswer: "קנדה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇿🇦",
+          options: ["קניה", "ניגריה", "דרום אפריקה", "אתיופיה"],
+          correctAnswer: "דרום אפריקה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇸🇪",
+          options: ["נורבגיה", "פינלנד", "דנמרק", "שבדיה"],
+          correctAnswer: "שבדיה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇦🇺",
+          options: ["ניו זילנד", "אוסטרליה", "פיג'י", "פפואה גינאה החדשה"],
+          correctAnswer: "אוסטרליה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇩🇪",
+          options: ["בלגיה", "גרמניה", "הולנד", "לוקסמבורג"],
+          correctAnswer: "גרמניה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇰🇷",
+          options: ["צפון קוריאה", "דרום קוריאה", "יפן", "טייוואן"],
+          correctAnswer: "דרום קוריאה"
+        }
+      ],
+      2: [
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇯🇵",
+          options: ["סין", "יפן", "וייטנאם", "קוריאה"],
+          correctAnswer: "יפן"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇮🇹",
+          options: ["ספרד", "צרפת", "איטליה", "יוון"],
+          correctAnswer: "איטליה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇧🇷",
+          options: ["ארצנטינה", "ברזיל", "קולומביה", "צ'ילה"],
+          correctAnswer: "ברזיל"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇬🇧",
+          options: ["בריטניה", "אוסטרליה", "ניו זילנד", "קנדה"],
+          correctAnswer: "בריטניה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇨🇦",
+          options: ["קנדה", "ארה״ב", "מקסיקו", "אלסקה"],
+          correctAnswer: "קנדה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇿🇦",
+          options: ["קניה", "ניגריה", "דרום אפריקה", "אתיופיה"],
+          correctAnswer: "דרום אפריקה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇸🇪",
+          options: ["נורבגיה", "פינלנד", "דנמרק", "שבדיה"],
+          correctAnswer: "שבדיה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇦🇺",
+          options: ["ניו זילנד", "אוסטרליה", "פיג'י", "פפואה גינאה החדשה"],
+          correctAnswer: "אוסטרליה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇩🇪",
+          options: ["בלגיה", "גרמניה", "הולנד", "לוקסמבורג"],
+          correctAnswer: "גרמניה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇰🇷",
+          options: ["צפון קוריאה", "דרום קוריאה", "יפן", "טייוואן"],
+          correctAnswer: "דרום קוריאה"
+        }
+      ],
+      3: [
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇱🇻",
+          options: ["לטביה", "אסטוניה", "ליטא", "בלארוס"],
+          correctAnswer: "לטביה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇰🇭",
+          options: ["וייטנאם", "קמבודיה", "לאוס", "מיאנמר"],
+          correctAnswer: "קמבודיה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇺🇾",
+          options: ["ארגנטינה", "אורוגוואי", "פרגוואי", "צ'ילה"],
+          correctAnswer: "אורוגוואי"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇧🇯",
+          options: ["טוגו", "בנין", "גאנה", "חוף השנהב"],
+          correctAnswer: "בנין"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇱🇰",
+          options: ["מלדיביים", "סרי לנקה", "בנגלדש", "נפאל"],
+          correctAnswer: "סרי לנקה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇧🇩",
+          options: ["פקיסטן", "בנגלדש", "מלזיה", "אינדונזיה"],
+          correctAnswer: "בנגלדש"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇰🇬",
+          options: ["קזחסטן", "קירגיזסטן", "טג'יקיסטן", "אוזבקיסטן"],
+          correctAnswer: "קירגיזסטן"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇧🇫",
+          options: ["מאלי", "בורקינה פאסו", "ניז'ר", "צ'אד"],
+          correctAnswer: "בורקינה פאסו"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇲🇳",
+          options: ["מונגוליה", "קזחסטן", "קירגיזסטן", "טיבט"],
+          correctAnswer: "מונגוליה"
+        },
+        {
+          question: "לאיזו מדינה שייך הדגל הזה? 🇹🇲",
+          options: ["אוזבקיסטן", "טורקמניסטן", "אזרבייג'ן", "טג'יקיסטן"],
+          correctAnswer: "טורקמניסטן"
+        }
+      ]
+    },
+    foods: {
+      1: [
+        {
+          question: "איזה מאכל מזוהה עם איטליה?",
+          options: ["סושי", "פיצה", "המבורגר", "פלאפל"],
+          correctAnswer: "פיצה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם יפן?",
+          options: ["סושי", "פסטה", "טאקוס", "קרואסון"],
+          correctAnswer: "סושי"
+        },
+        {
+          question: "איזה מאכל מזוהה עם מקסיקו?",
+          options: ["פאייה", "טאקוס", "סושי", "שניצל"],
+          correctAnswer: "טאקוס"
+        },
+        {
+          question: "איזה מאכל מזוהה עם צרפת?",
+          options: ["קרואסון", "פיצה", "המבורגר", "סושי"],
+          correctAnswer: "קרואסון"
+        },
+        {
+          question: "איזה מאכל מזוהה עם יוון?",
+          options: ["פלאפל", "סושי", "מוסקה", "פיצה"],
+          correctAnswer: "מוסקה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם הודו?",
+          options: ["סושי", "פיצה", "קארי", "המבורגר"],
+          correctAnswer: "��ארי"
+        },
+        {
+          question: "איזה מאכל מזוהה עם גרמניה?",
+          options: ["שניצל", "פיצה", "סושי", "טאקוס"],
+          correctAnswer: "שניצל"
+        },
+        {
+          question: "איזה מאכל מזוהה עם ספרד?",
+          options: ["פאייה", "פיצה", "סושי", "קרואסון"],
+          correctAnswer: "פאייה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם תאילנד?",
+          options: ["פאד תאי", "סושי", "פיצה", "קארי"],
+          correctAnswer: "פאד תאי"
+        },
+        {
+          question: "איזה מאכל מזוהה עם ארצות הברית?",
+          options: ["המבורגר", "פיצה", "סושי", "פלאפל"],
+          correctAnswer: "המבורגר"
+        }
+      ],
+      2: [
+        {
+          question: "איזה מאכל מזוהה עם וייטנאם?",
+          options: ["פו", "סושי", "פאד תאי", "דים סאם"],
+          correctAnswer: "פו"
+        },
+        {
+          question: "איזה מאכל מזוהה עם ספרד?",
+          options: ["פאייה", "פסטה", "קרואסון", "שניצל"],
+          correctAnswer: "פאייה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם מרוקו?",
+          options: ["טאג'ין", "פלאפל", "חומוס", "שווארמה"],
+          correctAnswer: "טאג'ין"
+        },
+        {
+          question: "איזה מאכל מזוהה עם שבדיה?",
+          options: ["קוטבולר", "שניצל", "המבורגר", "פיצה"],
+          correctAnswer: "קוטבולר"
+        },
+        {
+          question: "איזה מאכל מזוהה עם ברזיל?",
+          options: ["פייז'ואדה", "טאקוס", "פאייה", "פסטה"],
+          correctAnswer: "פייז'ואדה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם קוריאה?",
+          options: ["קימצ'י", "סושי", "פו", "דים סאם"],
+          correctAnswer: "קימצ'י"
+        },
+        {
+          question: "איזה מאכל מזוהה עם רוסיה?",
+          options: ["בורשט", "שניצל", "פירושקי", "גולאש"],
+          correctAnswer: "בורשט"
+        },
+        {
+          question: "איזה מאכל מזוהה עם הונגריה?",
+          options: ["גולאש", "שניצל", "פירושקי", "בורשט"],
+          correctAnswer: "גולאש"
+        },
+        {
+          question: "איזה מאכל מזוהה עם פרו?",
+          options: ["סביצ'ה", "טאקוס", "פאייה", "אמפנדה"],
+          correctAnswer: "סביצ'ה"
+        },
+        {
+          question: "איזה מאכל מזוהה עם אתיופיה?",
+          options: ["אינג'רה", "חומוס", "פלאפל", "שווארמה"],
+          correctAnswer: "אינג'רה"
+        }
+      ]
+    },
+    currencies: {
+      1: [
+        {
+          question: "מהו המטבע של ארצות הברית?",
+          options: ["אירו", "ין", "דולר", "פאונד"],
+          correctAnswer: "דולר"
+        },
+        {
+          question: "מהו המטבע של בריטניה?",
+          options: ["אירו", "פאונד", "דולר", "פרנק"],
+          correctAnswer: "פאונד"
+        },
+        {
+          question: "מהו המטבע של יפן?",
+          options: ["דולר", "וון", "יואן", "ין"],
+          correctAnswer: "ין"
+        },
+        {
+          question: "מהו המטבע של האיחוד האירופי?",
+          options: ["פרנק", "דולר", "אירו", "פאונד"],
+          correctAnswer: "אירו"
+        },
+        {
+          question: "מהו המט��ע של סין?",
+          options: ["ין", "וון", "יואן", "רופי"],
+          correctAnswer: "יואן"
+        },
+        {
+          question: "מהו המטבע של רוסיה?",
+          options: ["אירו", "רובל", "דולר", "זלוטי"],
+          correctAnswer: "רובל"
+        },
+        {
+          question: "מהו המטבע של קוריאה הדרומית?",
+          options: ["ין", "וון", "יואן", "רינגיט"],
+          correctAnswer: "וון"
+        },
+        {
+          question: "מהו המטבע של שוויץ?",
+          options: ["אירו", "דולר", "פרנק שוויצרי", "כתר"],
+          correctAnswer: "פרנק שוויצרי"
+        },
+        {
+          question: "מהו המטבע של הודו?",
+          options: ["דולר", "רופי", "יואן", "רובל"],
+          correctAnswer: "רופי"
+        },
+        {
+          question: "מהו המטבע של מקסיקו?",
+          options: ["אירו", "דולר", "ריאל", "פסו"],
+          correctAnswer: "פסו"
+        }
+      ],
+      2: [
+        {
+          question: "מהו המטבע של שבדיה?",
+          options: ["אירו", "כתר שבדי", "קרונה", "פאונד"],
+          correctAnswer: "כתר שבדי"
+        },
+        {
+          question: "מהו המטבע של ברזיל?",
+          options: ["פסו", "דולר", "ריאל", "אירו"],
+          correctAnswer: "ריאל"
+        },
+        {
+          question: "מהו המטבע של תאילנד?",
+          options: ["רינגיט", "רופי", "באט", "דונג"],
+          correctAnswer: "באט"
+        },
+        {
+          question: "מהו המטבע של טורקיה?",
+          options: ["דינר", "לירה טורקית", "אירו", "דולר"],
+          correctAnswer: "לירה טורקית"
+        },
+        {
+          question: "מהו המטבע של פולין?",
+          options: ["אירו", "כתר", "פורינט", "זלוטי"],
+          correctAnswer: "זלוטי"
+        },
+        {
+          question: "מהו המטבע של הונגריה?",
+          options: ["זלוטי", "פורינט", "אירו", "כתר"],
+          correctAnswer: "פורינט"
+        },
+        {
+          question: "מהו המטבע של דרום אפריקה?",
+          options: ["שילינג", "דולר", "ראנד", "פאונד"],
+          correctAnswer: "ראנד"
+        },
+        {
+          question: "מהו המטבע של נ��רבגיה?",
+          options: ["אירו", "קרונה", "כתר נורבגי", "כתר שבדי"],
+          correctAnswer: "כתר נורבגי"
+        },
+        {
+          question: "מהו המטבע של אינדונזיה?",
+          options: ["רינגיט", "באט", "פזו", "רופיה"],
+          correctAnswer: "רופיה"
+        },
+        {
+          question: "מהו המטבע של ערב הסעודית?",
+          options: ["דינר", "דירהם", "ריאל סעודי", "לירה"],
+          correctAnswer: "ריאל סעודי"
+        }
+      ]
+    }
+  };
